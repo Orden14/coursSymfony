7ebf5b5a9be09becd03ae6045e610f806358e609
Orden <t.lesrel@gmail.com>
2023-11-14 12:22:34 +0100

    setups

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -6,7 +6,6 @@ use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
-#[Route('/')]
 class HomeController extends AbstractController
 {
     #[Route('/', name: 'index')]
