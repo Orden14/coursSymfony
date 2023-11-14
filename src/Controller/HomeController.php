8a24b0c5326a1c24d72ba0c7fb69336c8b3f6b32
Orden <t.lesrel@gmail.com>
2023-11-14 09:33:25 +0100

    test

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -12,6 +12,8 @@ class HomeController extends AbstractController
     #[Route('/', name: 'index')]
     public function index(): Response
     {
+        $test = [1 => 'a', 2 => 'b'];
+        
         return $this->render('home/index.html.twig', [
             'projectName' => 'Cours Symfony',
         ]);
