6227e93cd83a0e6085b1634b024f8793e4adec5c
Orden14 <t.lesrel@gmail.com>
2023-11-16 12:20:12 +0100

    edit ContactFormType

--- src/Form/ContactFormType.php
+++ src/Form/ContactFormType.php
@@ -2,7 +2,9 @@
 
 namespace App\Form;
 
+use App\Entity\User;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Security\Core\Security;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\Validator\Constraints\NotBlank;
 use Symfony\Component\Form\Extension\Core\Type\EmailType;
@@ -10,10 +12,24 @@ use Symfony\Component\Form\Extension\Core\Type\TextareaType;
 
 class ContactFormType extends AbstractType
 {
+    public function __construct(
+        private readonly Security $security
+    ) {}
+
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
+        if($this->security->getUser()) {
+            
+            /**
+             * @var User $user
+             */
+            $user = $this->security->getUser();
+            $email = $user->getEmail();
+        }
+
         $builder
             ->add('email', EmailType::class, [
+                'data' => $email ?? null,
                 'constraints' => [
                     new NotBlank([
                         'message' => 'Vous devez entrer un email',
